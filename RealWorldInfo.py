abf664a026062fae14ad6b27afdd2965d6a69739
Will Compton <wdc3iii@gmail.com>
2019-02-21 21:29:01 -0500

    Assigned Random Data Assignment

--- RealWorldInfo.py
+++ RealWorldInfo.py
@@ -1,5 +1,7 @@
 import json
-
+import random
+#changes
+#assigning random values to random data in field_definition
 
 class RealWorldInfo:
     def __init__(self, screen_width_p, screen_height_p):
@@ -8,6 +10,14 @@ class RealWorldInfo:
         """
         with open('field_definition.json') as field_json:
             field_data = json.load(field_json)
+
+            for block in field_data:
+                if block.get("x") == "random":
+                    block["x"] = random() * field_data["Header"]["max_Width"]
+                if block.get("y") == "random":
+                    block["y"] = random() * field_data["Header"]["max_Height"]
+                if block.get("twist") == "random":
+                    block["twist"] = random.randrange(0,360)
 
         self.meters_to_pixels_vertical_scaling_factor = screen_height_p
         self.meters_to_pixels_horizontal_scaling_factor = screen_width_p
